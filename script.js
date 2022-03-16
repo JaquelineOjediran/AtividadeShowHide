5ace41c0e851e74d55e369f752ac11cd81a81bfc
Jaqueline Ojediran <86089010+JaquelineOjediran@users.noreply.github.com>
2022-03-16 09:53:08 -0300

    Inserção dos arquivos base
    Inclui HTML, CSS, JS e pasta de Imagens

---
+++ script.js
@@ -0,0 +1,27 @@
+function show_maca()
+{
+document.getElementById("maca").style.visibility="visible";
+document.getElementById("limao").style.visibility="hidden";
+document.getElementById("laranja").style.visibility="hidden";
+}
+
+function show_limao()
+{
+document.getElementById("maca").style.visibility="hidden";
+document.getElementById("limao").style.visibility="visible";
+document.getElementById("laranja").style.visibility="hidden";
+}
+
+function show_laranja()
+{
+document.getElementById("maca").style.visibility="hidden";
+document.getElementById("limao").style.visibility="hidden";
+document.getElementById("laranja").style.visibility="visible";
+}
+
+function some_tudo()
+{
+document.getElementById("maca").style.visibility="hidden";
+document.getElementById("limao").style.visibility="hidden";
+document.getElementById("laranja").style.visibility="hidden";
+}
